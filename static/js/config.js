48842c4413e4eaf8abb527c77daca55af956c981
Darren Miller <dazmiller@gmail.com>
2015-04-01 17:24:50 +1100

    added correct applicationID

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 angular.module('conference.config', [])
 
-    .constant('FB_APP_ID', '932536800093017') // Use your own FB app id
+    .constant('FB_APP_ID', '427826427392322') // Use your own FB app id
 
     .constant('SERVER_PATH','http://conferenceapp.azurewebsites.net/');
 //    .constant('SERVER_PATH','http://ioconf.herokuapp.com');
